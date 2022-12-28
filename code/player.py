99744268e24c26c48cdb27ab949c3f6d0fcd80de
warcholjakub <warcholljakub@gmail.com>
2022-12-28 16:08:03 +0100

    Kind of working camera.

--- code/player.py
+++ code/player.py
@@ -15,6 +15,7 @@ class Player(pygame.sprite.Sprite):
 
         self.image = self.animations[self.status][self.frame_index]
         self.rect = self.image.get_rect(center=position)
+        self.z = LAYERS['main']
 
         self.direction = pygame.math.Vector2(0, 0)
         # !
--- code/settings.py
+++ code/settings.py
@@ -4,4 +4,17 @@ SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 TILE_SIZE = 64
 
+LAYERS = {
+	'water': 0,
+	'ground': 1,
+	'soil': 2,
+	'soil water': 3,
+	'rain floor': 4,
+	'house bottom': 5,
+	'ground plant': 6,
+	'main': 7,
+	'house top': 8,
+	'fruit': 9,
+	'rain drops': 10
+}
 # Wywaliłem to bo póki co jest zbędne i tak bedzie zmieniane, jak sie wszystko wywali to wtedy bedziemy sie martwic xdd
